ffa30ad16f3f9e9cdac1e9641baf6d2aced32476
mattspeidel <mattspeidel@gmail.com>
2019-06-20 13:33:45 -0400

    add models

--- miniblog/catalog/models.py
+++ miniblog/catalog/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from datetime import date, time
 
 # Create your models here.
+
+class Blog(models.Model):
+
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Blogger', on_delete=models.SET_NULL, null=True)
+    description = models.TextField(max_length=1000, help_text='Enter your blog text')
+    post_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    # def get_absolute_url(self):
+    #     """FIX ME Returns the url to access a detail record for this post"""
+    #     return reverse('book-detail', args=[str(self.id)])
+
+class Blogger(models.Model):
+
+    name = models.CharField(max_length=100)
+    bio = models.TextField(max_length=1000, help_text='Enter a bio')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    # def get_absolute_url(self):
+    #     """FIX ME Returns the url to access a particular author instance."""
+    #     return reverse('author-detail', args=[str(self.id)])
+
+class Comment(models.Model):
+
+    name = models.CharField(max_length=100)
+    post_date = models.DateField(null=True, blank=True)
+    post_time = models.TimeField(null=True, blank=True)
+    coment = models.TextField(max_length=300, help_text='Enter your comment')
+
+    def __str__(self):
+        return f'{self.name}'
+    
